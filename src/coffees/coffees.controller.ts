cfc2d4fa39d41895f81813982cd121a55f702a2c
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-27 11:40:50 +0200

    adding findOne() GET method + route parameters

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
+  @Get()
   findAll() {
-    return 'Retrieving all coffees flavors, what else?';
+    return 'Retrieving all coffees, what else?';
+  }
+
+  /* @Get(':id')
+  findOne(@Param() params) {
+    return `Retrieving coffee #${params.id}, what else?`;
+  } */
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Retrieving coffee #${id}, what else?`;
   }
 }
