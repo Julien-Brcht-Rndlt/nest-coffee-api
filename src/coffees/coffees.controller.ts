7a837e6458f30d73e63ef4b332a5811a3197c24c
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-27 13:00:20 +0200

    adding DELETE, PATCH, PUT operations for modifying / deleting coffees

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,20 +1,31 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
+  Put,
   Res,
 } from '@nestjs/common';
+import { identity } from 'rxjs';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get()
+  /* @Get()
   findAll(@Res() response) {
+    // To be used with caution as loosing NestJS abstraction + compatibility with
+    // NestJS request/response handling function + becoming
     response.status(200).send('Retrieving all coffees, what else?');
-    /* return 'Retrieving all coffees, what else?'; */
+    //return 'Retrieving all coffees, what else?';
+  } */
+
+  @Get()
+  findAll() {
+    return 'Retrieving all coffees, what else?';
   }
 
   /* @Get(':id')
@@ -32,11 +43,34 @@ export class CoffeesController {
     return `Creating a new coffee: ${body}`;
   } */
 
-  @Post()
+  /* @Post()
   @HttpCode(HttpStatus.OK)
   create(@Body() body) {
     return body;
-    /* return `Creating a new coffee: ${body.name}
-         with a price of $${body.price.toFixed(2)}`; */
+    //return `Creating a new coffee: ${body.name}
+    //    with a price of $${body.price.toFixed(2)}`;
+  } */
+
+  @Post()
+  create(@Body() body) {
+    return body;
+    //return `Creating a new coffee: ${body.name}
+    //    with a price of $${body.price.toFixed(2)}`;
+  }
+
+  @Patch(':id')
+  updatePart(@Param('id') id: string, @Body() body) {
+    return `Updating partially coffee #${id}, what else?`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `Updating coffee #${id}, what else?`;
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return `Removing coffee #${id}, what else?`;
   }
 }
