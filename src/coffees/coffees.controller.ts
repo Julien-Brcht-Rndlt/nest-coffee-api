f93c4b2cbcb5cd00812957a9e2b3dc6c8529e10c
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-27 12:29:07 +0200

    Response Status Code Decorators + Express native response handling

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'Retrieving all coffees, what else?';
+  findAll(@Res() response) {
+    response.status(200).send('Retrieving all coffees, what else?');
+    /* return 'Retrieving all coffees, what else?'; */
   }
 
   /* @Get(':id')
@@ -23,6 +33,7 @@ export class CoffeesController {
   } */
 
   @Post()
+  @HttpCode(HttpStatus.OK)
   create(@Body() body) {
     return body;
     /* return `Creating a new coffee: ${body.name}
