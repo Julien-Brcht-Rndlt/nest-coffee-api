2c1d0a16723aab64720aa526b82c9f01032f87cf
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-27 15:54:47 +0200

    using HttpException ou NotFoundException to handle user-friendly errors

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -17,8 +17,20 @@ export class CoffeesService {
     this.coffees.push(createCoffeeDto);
   }
 
-  readOne(id: number): Coffee | undefined {
-    return this.coffees.find((coffee) => coffee.id === id);
+  /* readOne(id: number): Coffee {
+    const coffee: Coffee = this.coffees.find((coffee) => coffee.id === id);
+    if (!coffee) {
+      throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+    }
+    return coffee;
+  } */
+
+  readOne(id: number): Coffee {
+    const coffee: Coffee = this.coffees.find((coffee) => coffee.id === id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+    return coffee;
   }
 
   readAll(): Coffee[] {
