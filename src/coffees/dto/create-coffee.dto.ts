990e14b704789ecf59ff7ea49b89b2cea146c75e
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-30 15:28:12 +0200

    adding more infos on DTO properties

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,15 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'The name of a coffee. What else?' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'The coffee brand. What else?' })
   @IsString()
   readonly brand: string;
 
+  @ApiProperty({ description: 'The coffee price. What else?' })
   @IsNumber()
   readonly price: number;
 
+  @ApiProperty({ example: [] })
   @IsString({ each: true })
   readonly flavors: string[];
 }
