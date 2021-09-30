8c56c0169a84a44b168d4300b9195c866fafe6c7
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-30 14:55:44 +0200

    install + settings SwaggerModule for OpenAPI Specification + generate documentation end-points, DTOs, Entities..

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
@@ -28,6 +29,16 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     new TimeoutInterceptor(),
   );
+
+  //OpenAPI Specification settings
+  const options = new DocumentBuilder()
+    .setTitle('Coffee-API')
+    .setDescription('An API about Coffee for Coffee Lovers')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(8080);
 }
